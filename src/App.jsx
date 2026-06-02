d25c7dbd15f34fe39a0eb18c187490341638680e
nairx <nairxprv@gmail.com>
2026-06-02 16:35:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function Header() {
+  return <div>Header</div>;
+}
+
+function Footer() {
+  return <div>Footer</div>;
+}
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function App() {
+  return (
+    <div>
+     <Header/>
+     <Home/>
+     <Footer/>
+    </div>
+  );
+}
+export default App;
