60621e01819e50c201d77c39d9aae93a1f425771
nairx <nairxprv@gmail.com>
2026-06-03 12:36:15 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,66 @@
 import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function App() {
-  const [user, setUser] = useState({});
-  const [message, setMessage] = useState();
-  const users = [
-    { id: 1, email: "john@gmail.com", password: "1234" },
-    { id: 2, email: "amy@gmail.com", password: "4567" },
-  ];
-  const handleLogin = () => {
-    const found = users.find(
-      (element) =>
-        element.email === user.email && element.password === user.password,
-    );
-    if (found) {
-      setMessage("Welcome");
-    } else {
-      setMessage("Access Denied");
-    }
+  const [products, setProducts] = useState([]);
+  const API_URL = "http://localhost:5000/products";
+  const fetchProducts = async () => {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    setProducts(data);
   };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
     <div>
-      <h2>Login Form</h2>
-      {message}
-      <p>
-        <input
-          type="text"
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          placeholder="Email"
-        />
-      </p>
-      <p>
-        <input
-          type="password"
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          placeholder="Password"
-        />
-      </p>
-      <button onClick={handleLogin}>Login</button>
+      {products &&
+        products.map((product) => <li key={product.id}>{product.name}</li>)}
     </div>
   );
 }
+
+// import React from "react";
+// import { useState } from "react";
+// export default function App() {
+//   const [user, setUser] = useState({});
+//   const [message, setMessage] = useState();
+//   const users = [
+//     { id: 1, email: "john@gmail.com", password: "1234" },
+//     { id: 2, email: "amy@gmail.com", password: "4567" },
+//   ];
+//   const handleLogin = () => {
+//     const found = users.find(
+//       (element) =>
+//         element.email === user.email && element.password === user.password,
+//     );
+//     if (found) {
+//       setMessage("Welcome");
+//     } else {
+//       setMessage("Access Denied");
+//     }
+//   };
+//   return (
+//     <div>
+//       <h2>Login Form</h2>
+//       {message}
+//       <p>
+//         <input
+//           type="text"
+//           onChange={(e) => setUser({ ...user, email: e.target.value })}
+//           placeholder="Email"
+//         />
+//       </p>
+//       <p>
+//         <input
+//           type="password"
+//           onChange={(e) => setUser({ ...user, password: e.target.value })}
+//           placeholder="Password"
+//         />
+//       </p>
+//       <button onClick={handleLogin}>Login</button>
+//     </div>
+//   );
+// }
 
 // import React, { useState } from "react";
 // export default function App() {
