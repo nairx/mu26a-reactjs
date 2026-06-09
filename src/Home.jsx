87d3a17bb5bcfc5cb6c9701f4e479ea4541364a0
nairx <nairxprv@gmail.com>
2026-06-09 10:52:29 +0530

    updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,31 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "./App";
+import "./Home.css"
+import axios from "axios";
 export default function Home() {
   const { user } = useContext(AppContext);
+  const [products, setProducts] = useState([]);
+  const url = (import.meta.env.VITE_API_URL + "/products");
+  const fetchProducts = async () => {
+    const res = await axios.get(url + "/get");
+    setProducts(res.data.products)
+  };
+  useEffect(()=>{
+    fetchProducts()
+  },[])
   return (
-    <div>
-      Home
-      <h2>{user?.name}</h2>
+    <div className="row">
+        {products && products.map(product=>(
+        <div key={product._id}>
+          <img src={product.imgUrl}/>
+          <h3>{product.name}</h3>
+          <p>{product.description}</p>
+          <h4>{product.price}</h4>
+          <p>
+            <button>Add to Cart</button>
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
