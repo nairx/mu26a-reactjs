37ee6d7d73a4f3aebcc81a3d75656ceab10847e0
nairx <nairxprv@gmail.com>
2026-06-09 16:07:57 +0530

    updated

--- src/Orders.jsx
+++ src/Orders.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 export default function Orders() {
+  const [orders, setOrders] = useState([]);
+  const url = import.meta.env.VITE_API_URL + "/orders";
+  const fetchOrders = async () => {
+    const res = await axios.get(`${url}/get`);
+    setOrders(res.data.orders);
+  };
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+  const delivered = async (orderId) => {
+    const res = await axios.patch(`${url}/update/${orderId}`);
+    fetchOrders();
+  };
   return (
-    <div>Orders</div>
-  )
+    <div>
+      Orders
+      <ol>
+        {orders &&
+          orders.map((order) => (
+            <li key={order._id}>
+              {order._id}-{order.email}-{order.orderValue}-{order.orderStatus}-
+              <button onClick={() => delivered(order._id)}>Delivered</button>
+            </li>
+          ))}
+      </ol>
+      <hr />
+    </div>
+  );
 }
