71fb3c844366cc665ede94a8aa525c4225fd56ad
nairx <nairxprv@gmail.com>
2026-06-09 13:26:52 +0530

    updated

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import { AppContext } from './App'
+import { useContext } from 'react'
 export default function Cart() {
+  const {cart,setCart,user} = useContext(AppContext)
   return (
-    <div>Cart</div>
+    <div>My Cart
+{cart && cart.map(item=>(
+  <div key={item._id}>{item.name}-{item.price}-{item.quantity}</div>
+))}
+
+
+    </div>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,31 +1,35 @@
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from "./App";
-import "./Home.css"
+import "./Home.css";
 import axios from "axios";
 export default function Home() {
-  const { user } = useContext(AppContext);
+  const { user, cart,setCart } = useContext(AppContext);
   const [products, setProducts] = useState([]);
-  const url = (import.meta.env.VITE_API_URL + "/products");
+  const url = import.meta.env.VITE_API_URL + "/products";
   const fetchProducts = async () => {
     const res = await axios.get(url + "/get");
-    setProducts(res.data.products)
+    setProducts(res.data.products);
   };
-  useEffect(()=>{
-    fetchProducts()
-  },[])
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  const addToCart = (product) => {
+    setCart([...cart, {...product,quantity:1}]);
+  };
   return (
     <div className="row">
-        {products && products.map(product=>(
-        <div key={product._id}>
-          <img src={product.imgUrl} width={300}/>
-          <h3>{product.name}</h3>
-          <p>{product.description}</p>
-          <h4>{product.price}</h4>
-          <p>
-            <button>Add to Cart</button>
-          </p>
-        </div>
-      ))}
+      {products &&
+        products.map((product) => (
+          <div key={product._id}>
+            <img src={product.imgUrl} width={300} />
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <h4>{product.price}</h4>
+            <p>
+              <button onClick={()=>addToCart(product)}>Add to Cart</button>
+            </p>
+          </div>
+        ))}
     </div>
   );
 }
